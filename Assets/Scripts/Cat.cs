e77cf351b1777f37b4d96f8321f4250f7af0111a
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-05-17 11:47:47 +0800

    Initial Commit

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public float thrust;
+    private Rigidbody2D catRigidbody;
+
+    void Awake()
+    {
+        catRigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // transform.Translate(transform.right * Time.deltaTime * thrust);
+        catRigidbody.AddForce(transform.right * thrust);
+    }
+}
---
+++ Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler,IPointerDownHandler, IPointerUpHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("Drag");
+        transform.position = Camera.main.ScreenToWorldPoint(
+            new Vector3(
+                Input.mousePosition.x,
+                Input.mousePosition.y,
+                Camera.main.transform.position.z * -1
+            )
+        );
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Down");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Up");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+    }
+}
